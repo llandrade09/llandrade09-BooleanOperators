d5c96bc32b91204eec5791b37eff888a423e75a6
Luis Andrade <santiago16999@gmail.com>
2023-02-28 07:22:48 +0000

    passed without using if/else

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,7 +14,7 @@ public class Lab {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        return false;
+        return b;
     }
 
     /**
@@ -23,9 +23,8 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+       return !b;
     }
-
     /**
      * @param b1 a true/false value.
      * @param b2 a true/false value.
@@ -33,7 +32,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+        return b1&b2;
     }
 
     /**
@@ -43,7 +42,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
+        return b1|b2;
     }
 
 
